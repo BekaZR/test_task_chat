4fd20536e8f02a73b17bdc803922e6e8452e31ab
Beknazar Abdyldaev <abdyldaev.duff@gmail.com>
2022-10-31 16:55:22 +0600

    add room templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
-from django.shortcuts import render
-
 
 def index(request):
     return render(request, "mainapp/index.html")
+
+
+def room(request, room_name):
+    return render(request, "mainapp/room.html", {"room_name": room_name})
